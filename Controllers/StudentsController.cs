be232521070a61b2bbb1f1c20da62c820998e844
Sachee <sacheeghimire@gmail.com>
2025-07-04 21:23:31 +0545

    Applying async and await function in StudentController

--- Controllers/StudentsController.cs
+++ Controllers/StudentsController.cs
@@ -1,18 +1,43 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using student_portal.Data;
 using student_portal.Models;
+using student_portal.Models.Entities;
+//using System.ComponentModel.DataAnnotations;
+//using System.Runtime.CompilerServices;
 
 namespace student_portal.Controllers
 {
     public class StudentsController : Controller
     {
+        private readonly ApplicationDbContext dbContext;
+        public StudentsController(ApplicationDbContext dbContext)
+        {
+           this.dbContext = dbContext;
+        }
+
+     
+
         [HttpGet]
         public IActionResult Add()
         {
             return View();
         }
         [HttpPost]
-        public IActionResult Add(AddStudentViewModel viewModel)
+
+        // Making Asynchronous
+        public async Task<IActionResult> Add(AddStudentViewModel viewModel)
         {
+            var student =new Student
+            {
+                Name = viewModel.Name,
+                Email = viewModel.Email,
+                Phone = viewModel.Phone,
+                Subscribed = viewModel.Subscribed
+            };
+
+            await dbContext.Students.AddAsync(student);
+            await dbContext.SaveChangesAsync();
+
 
             return View();
         }
